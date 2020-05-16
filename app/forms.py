b6f7a0001f99ec4b71674f0ac0cf8c1fdd81e771
Gamebrewser <44047031+Gamebrewser@users.noreply.github.com>
2020-05-16 00:07:28 -0500

    Updated

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,30 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileRequired
 from wtforms import TextField, FileField, StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 from werkzeug.utils import secure_filename
 
 
 class RegisterForm(FlaskForm):
-	Username = StringField('Username',validatiors=[DataRequired])
-	Password = PasswordField('Password',validatiors=[DataRequired])
-	Firstname = StringField('Firstname',validatiors=[DataRequired])
-	Lastname = StringField('Lastname',validatiors=[DataRequired])
-	Email = StringField('email',validatiors=[DataRequired])
-	Location = StringField('location',validatiors=[DataRequired])
+	Username = StringField('Username',validators=[DataRequired])
+	Password = PasswordField('Password',validators=[DataRequired])
+	Firstname = StringField('Firstname',validators=[DataRequired])
+	Lastname = StringField('Lastname',validators=[DataRequired])
+	Email = StringField('email',validators=[DataRequired])
+	Location = StringField('location',validators=[DataRequired])
 	Biography = TextAreaField('Biography',validators=[DataRequired])
   photo = FileField('ProfilePhoto', validators=[FileRequired(), FileAllowed('jpg','png','jpeg')])
 
+class LoginForm(FlaskForm):
+  Username = StringField('Username', validators=[InputRequired()])
+  Password = PasswordField('Password',validators=[InputRequired()])
 
+class PostForm(FlaskForm):
+  user_id = StringField("", validators=[DataRequired()])
+  photo = FileField("ProfilePhoto", validators=[DataRequired()])
+  caption = TextAreaField("Caption: ")
+
+#not sure if the caption needed a validator and not too sure if DataRequired must end with the () like InputRequired(). - Kyle
 
 
 #class UploadForm(FlaskForm):
