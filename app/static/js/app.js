2c21f2ec60c4d1d9232235fd455ade0c4f515db4
NaldoRonz <ronaldomreid19@gmail.com>
2020-05-15 22:27:38 -0500

    Base

---
+++ app/static/js/app.js
@@ -0,0 +1,107 @@
+/* Add your Application JavaScript */
+Vue.component('app-header', {
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <a class="navbar-brand" href="#">Project 2</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/upload">Upload <span class="sr-only">(current)</span></router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    `
+});
+
+Vue.component('app-footer', {
+    template: `
+    <footer>
+        <div class="container">
+            <p>Copyright &copy; Flask Inc.</p>
+        </div>
+    </footer>
+    `
+});
+
+const Home = Vue.component('home', {
+   template: `
+    <div class="jumbotron">
+        <h1>Lab 7</h1>
+        <p class="lead">In this lab we will demonstrate VueJS working with Forms and Form Validation from Flask-WTF.</p>
+    </div>
+   `,
+    data: function() {
+       return {}
+    }
+});
+
+const Upload = Vue.component('upload-form', {
+   template: `
+    <div class="my_form">
+      <form id = "app" action = "api/upload" enctype = "multipart/form-data" method = "post">
+          <div class = "my_form">
+            <p>
+              <label for = "Description">Description</label>
+            </p>
+            <p>
+              <input id ="Description" v-model = "Description" type = "text">
+            </p> </br>
+            <p>
+              <input type="file" v-on:change="handleFileUpload()" key = "Photo-input">
+            </p>
+            <button v-bind:Photo="file" v-bind:Description="Description" @click="uploadPhoto"> Submit</button>
+          </div>
+        </form>  
+    </div>
+   `,
+
+    data() {
+       return {
+
+          Descriptions:[
+          ],
+          File:[]
+       }
+    }
+});
+
+
+
+const NotFound = Vue.component('not-found', {
+    template: `
+    <div>
+        <h1>404 - Not Found</h1>
+    </div>
+    `,
+    data: function () {
+        return {}
+    }
+})
+
+// Define Routes
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {path: "/", component: Home},
+        // Put other routes here
+
+        {path: "/upload", component: Upload},
+
+        // This is a catch all route in case none of the above matches
+        {path: "*", component: NotFound}
+    ]
+});
+
+// Instantiate our main Vue Instance
+const app = new Vue({
+    el: "#app",
+    router
+});
